cd4743e5952e44ca03cd333e04f07f7b52b86301
Petr Romanov <MucTop@yandex.ru>
2023-04-12 09:56:38 +0300

    update10

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,7 +4,7 @@ import getRandomNum from '../helpers/get-random-from-range.js';
 const generateProgression = (firstNum, step, length) => {
   const numbers = [];
   let numberRow = firstNum;
-  for (let i = 0; i <= length; i += 1) {
+  for (let i = 0; i < length; i += 1) {
     numbers.push(numberRow);
     numberRow += step;
   }
@@ -18,7 +18,7 @@ const progression = () => {
   const progrStep = getRandomNum(20);
   const progrLength = getRandomNum((20 - 5), 5);
   const fullProgression = generateProgression(firstNum, progrStep, progrLength);
-  const numberToHide = getRandomNum(fullProgression.length);
+  const numberToHide = getRandomNum(fullProgression.length - 1);
   const correctAnswer = String(fullProgression[numberToHide]);
   fullProgression[numberToHide] = '..';
   const expression = fullProgression.join(' ');
